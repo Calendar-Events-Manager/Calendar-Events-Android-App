d68cc9026a211eab8bf4b9fa12306584ecb40baa
Prasanna Anbazhagan <praslnx8@gmail.com>
2020-01-12 12:42:22 +0530

    Add logic to show more than one day events.

--- app/src/main/java/com/mymeetings/android/utils/ClockUtils.kt
+++ app/src/main/java/com/mymeetings/android/utils/ClockUtils.kt
@@ -11,12 +11,21 @@ class ClockUtils {
     fun getTimeLeft(timeInMillis : Long): String = DateUtils.getRelativeTimeSpanString(timeInMillis).toString()
 
     fun getTimeInHoursAndMins(timeInMillis: Long): String {
-        val calendar = Calendar.getInstance()
-        calendar.timeInMillis = timeInMillis
+        val todayCalendar = Calendar.getInstance()
+        todayCalendar.set(Calendar.HOUR_OF_DAY, 0)
+        todayCalendar.set(Calendar.MINUTE, 0)
+        todayCalendar.set(Calendar.SECOND, 0)
+        todayCalendar.set(Calendar.MILLISECOND, 0)
 
-        val hour = calendar.get(Calendar.HOUR)
-        val min = calendar.get(Calendar.MINUTE)
-        val amOrPm = calendar.get(Calendar.AM_PM)
+        val eventCalendar = Calendar.getInstance()
+        eventCalendar.timeInMillis = timeInMillis
+
+        val dayFromToday = ((eventCalendar.timeInMillis-todayCalendar.timeInMillis)/TimeUnit.DAYS.toMillis(1)).toInt()
+        val hour = eventCalendar.get(Calendar.HOUR)
+        val min = eventCalendar.get(Calendar.MINUTE)
+        val amOrPm = eventCalendar.get(Calendar.AM_PM)
+
+        val dayFromTodayStr = if(dayFromToday == 0) "" else "(${dayFromToday.toString()}) "
         val hourString = getFormattedTimePart(hour, true)
         val minString = getFormattedTimePart(min, false)
         val amOrPmString = if(amOrPm == Calendar.AM) {
@@ -25,7 +34,7 @@ class ClockUtils {
             "PM"
         }
 
-        return "$hourString:$minString $amOrPmString"
+        return "$dayFromTodayStr$hourString:$minString $amOrPmString"
     }
 
     private fun getFormattedTimePart(number: Int, isHour : Boolean) = when {
