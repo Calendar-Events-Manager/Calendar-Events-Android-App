eba5dc569ba777a3b6fe4a08b8dd77f3b4902e74
Prasanna Anbazhagan <praslnx8@gmail.com>
2020-01-23 08:50:50 +0530

    Add unit test for LocalCalendarChangeEventReciever

--- app/src/main/java/com/mymeetings/android/debug/ConsoleLog.kt
+++ app/src/main/java/com/mymeetings/android/debug/ConsoleLog.kt
@@ -3,24 +3,22 @@ package com.mymeetings.android.debug
 import android.util.Log
 import com.mymeetings.android.BuildConfig
 
-object ConsoleLog {
-
-    val isDebug = BuildConfig.DEBUG
+class ConsoleLog(private val enableLog : Boolean) {
 
     fun i(tag : String? = "APP", message : String) {
-        if(isDebug) {
+        if(enableLog) {
             Log.i(tag, message)
         }
     }
 
     fun d(tag : String? = "APP", message : String) {
-        if(isDebug) {
+        if(enableLog) {
             Log.d(tag, message)
         }
     }
 
     fun w(tag : String? = "APP", message : String) {
-        if(isDebug) {
+        if(enableLog) {
             Log.w(tag, message)
         }
     }
