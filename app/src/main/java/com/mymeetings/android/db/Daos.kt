37e8b55c30a0d44f672521f12dd17bb42967bf9f
Prasanna Anbazhagan <praslnx8@gmail.com>
2020-02-01 00:11:55 +0530

    ISSUE-11 : Refactor view model and introduce UI model for view component. It fixes the law of delimiter smell.

--- app/src/main/java/com/mymeetings/android/db/Daos.kt
+++ app/src/main/java/com/mymeetings/android/db/Daos.kt
@@ -6,17 +6,17 @@ import androidx.room.*
 interface CalendarEventsDao {
 
     @Query("SELECT * FROM calendar_events where end_time > :givenTime ORDER BY start_time ASC LIMIT 100")
-    suspend fun getCalendarEventsEndingAfter(givenTime : Long): List<CalendarEventDbModel>
+    fun getCalendarEventsEndingAfter(givenTime : Long): List<CalendarEventDbModel>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun addCalendarEvent(calendarEvent: CalendarEventDbModel): Long
+    fun addCalendarEvent(calendarEvent: CalendarEventDbModel): Long
 
     @Update
-    suspend fun updateCalendarEvent(calendarEvent: CalendarEventDbModel) : Int
+    fun updateCalendarEvent(calendarEvent: CalendarEventDbModel) : Int
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun addCalendarEvents(calendarEvents : List<CalendarEventDbModel>) : LongArray
+    fun addCalendarEvents(calendarEvents : List<CalendarEventDbModel>) : LongArray
 
     @Query("DELETE FROM calendar_events")
-    suspend fun purgeCalendarEvents()
+    fun purgeCalendarEvents()
 }
